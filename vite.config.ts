4ecd73a63ab6510b6c4541cfbab0d733dde7f7b9
Ethan <85222153+EthanNguyen13@users.noreply.github.com>
2026-06-01 22:15:51 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-    base: '/PROJECT_LOGAPP/', 
+    base: 'PROJECT_LOGAPP', 
       },
     },
     server: {
